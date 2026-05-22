78da4cc536b320807072570ccbc968eda55d573f
bheluga <bheluga@defillama.com>
2026-05-22 20:28:14 +0530

    map polymarket active addresses (#12089)

--- defi/src/protocols/data1.ts
+++ defi/src/protocols/data1.ts
@@ -13512,7 +13512,8 @@ The eWIT token is a custodial, wrapped version of the Witnet coin managed by the
     dimensions: {
       fees: "polymarket",
       dexs: "polymarket",
-      "open-interest": "polymarket-oi"
+      "open-interest": "polymarket-oi",
+      "active-users": "polymarket",
     },
     hallmarks: [
       ["2026-01-06", "Fee introduced in 15-minute crypto markets"],

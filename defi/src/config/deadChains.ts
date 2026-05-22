4306ace08fb6b266e1a23ca76de6afbce47d2d73
g1nt0ki <99907941+g1nt0ki@users.noreply.github.com>
2026-05-22 10:25:52 +0200

    redstone chain is dead

--- defi/src/config/deadChains.ts
+++ defi/src/config/deadChains.ts
@@ -16,6 +16,7 @@ export const deadChains = [
   'titan',  // https://tokenize.exchange/ the entity behind it is insolvent?
   'mint', // shutdown 2026-04-17: https://x.com/Mint_Blockchain/status/2044980026819617147
   'evmos', // shutdown 2026-05-18: https://x.com/Disperze_/status/2054625049429127192
+  'redstone', // shutdown 2026-05-18: https://x.com/redstonexyz/status/2055181780043579468
 ]
 
 

0fd0fdda3f89d34802f49aa4a68822c39e95f72f
Tom <84812080+0xtomm@users.noreply.github.com>
2026-06-06 00:35:38 +0100

    chore(chains): add missing token to chains (#11754)

--- defi/src/utils/normalizeChain.ts
+++ defi/src/utils/normalizeChain.ts
@@ -1054,10 +1054,10 @@ export const chainCoingeckoIds = {
     },
   },
   "Ronin": {
-    geckoId: null,
+    geckoId: "ronin",
     github: ["axieinfinity"],
     symbol: "RON",
-    cmcId: null,
+    cmcId: "14101",
     categories: ["EVM"],
     chainId: 2020,
     twitter: "Ronin_Network",
@@ -2679,9 +2679,9 @@ export const chainCoingeckoIds = {
     },
   },
   "Beam": {
-    geckoId: null,
-    symbol: "MC",
-    cmcId: null,
+    geckoId: "beam-2",
+    symbol: "BEAM",
+    cmcId: "28298",
     categories: ["EVM"],
     parent: {
       chain: "Avalanche",
@@ -3491,9 +3491,9 @@ export const chainCoingeckoIds = {
     chainId: 111188,
   },
   "Taiko": {
-    geckoId: null,
-    symbol: null,
-    cmcId: null,
+    geckoId: "taiko",
+    symbol: "TAIKO",
+    cmcId: "31525",
     categories: ["EVM", "Rollup"],
     parent: {
       chain: "Ethereum",
@@ -3618,9 +3618,9 @@ export const chainCoingeckoIds = {
     },
   },
   "Rari": {
-    geckoId: null,
-    symbol: null,
-    cmcId: null,
+    geckoId: "rarible",
+    symbol: "RARI",
+    cmcId: "5877",
     categories: ["EVM", "Arbitrum Chains"],
     parent: {
       chain: "Arbitrum",
@@ -4975,9 +4975,9 @@ export const chainCoingeckoIds = {
     twitter: "kamigotchiworld",
   },
   "Katana": {
-    geckoId: null,
-    symbol: null,
-    cmcId: null,
+    geckoId: "katana-network-token",
+    symbol: "KAT",
+    cmcId: "38769",
     categories: ["EVM"],
     twitter: "katana",
     url: "https://katana.network/",
